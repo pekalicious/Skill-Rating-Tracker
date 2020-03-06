fb7b16d398629611d2b6f8e0faaaac51db0935a2
Panagiotis Peikidis <pekalicious@gmail.com>
2020-03-05 23:02:45 -0500

    Created separate page to start a new session

---
+++ SR Tracker/Views/StartSessionPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Pekalicious.SrTracker.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StartSessionPage : ContentPage
+    {
+        public StartSessionPage()
+        {
+            InitializeComponent();
+        }
+
+        async void StartSessionButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CurrentSessionPage());
+        }
+    }
+}
