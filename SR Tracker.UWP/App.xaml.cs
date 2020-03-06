f9753540c073c950b212cb8adfe54954068d1dec
Panagiotis Peikidis <pekalicious@gmail.com>
2020-03-05 23:44:57 -0500

    Fixed UWP after refactor

--- SR Tracker.UWP/App.xaml.cs
+++ SR Tracker.UWP/App.xaml.cs
@@ -15,7 +15,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-namespace SR_Tracker.UWP
+namespace Pekalicious.SrTracker.UWP
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
--- SR Tracker.UWP/MainPage.xaml.cs
+++ SR Tracker.UWP/MainPage.xaml.cs
@@ -13,7 +13,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-namespace SR_Tracker.UWP
+namespace Pekalicious.SrTracker.UWP
 {
     public sealed partial class MainPage
     {
@@ -21,7 +21,7 @@ namespace SR_Tracker.UWP
         {
             this.InitializeComponent();
 
-            LoadApplication(new SR_Tracker.App());
+            LoadApplication(new SrTracker.App());
         }
     }
 }
